183d7c7150c3410be8d14bf19c9377a3825606fd
GrandZizi <josmarbm@gmail.com>
2021-03-11 09:11:33 -0500

    re-roll button displays new strains but does not display on page load

--- src/Components/Recommended.js
+++ src/Components/Recommended.js
@@ -3,25 +3,27 @@ import React, { useEffect, useState } from 'react';
 function Recommended({ strainData }) {
 	const [selectedStrains, setSelectedStrains] = useState([]);
 
+
 	useEffect(() => {
-		randomStrains();
-		setSelectedStrains([]);
-	}, [selectedStrains]);
+		setSelectedStrains(randomStrains())
+	}, []);
 
 	function randomStrains() {
 		let randomizedStrains = [];
 		const strainDataKeys = Object.keys(strainData);
+		console.log(strainData);
 
 		for (let i = 0; i < 6; i++) {
 			let randomIndex = Math.floor(Math.random() * 1970);
 			randomizedStrains.push(strainDataKeys[randomIndex]);
 		}
+
+		return randomizedStrains;
 	}
-	randomStrains();
 
 	return (
 		<div>
-			<button onClick={randomStrains}>Re-roll</button>
+			<button onClick={() => setSelectedStrains(randomStrains())}>Re-roll</button>
 			<h2>Recommended Strains</h2>
 			{selectedStrains.map((strain) => (
 				<h4>{strain}</h4>
